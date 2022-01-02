572e918d38b1fe6b94669a2b411a128c2553fd26
Pijush Barik <pijush.barik8@gmail.com>
2022-01-02 22:49:09 +0530

    code refactorings and tailwindcss init

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Container from "../ui/Container"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <Container fluid className="flex justify-between items-center py-4">
+      <Link to="/" className="text-red-500 font-bold text-4xl">
+        {siteTitle}
+      </Link>
+      <nav>
+        <ul className="flex items-center space-x-4">
+          <li>
+            <Link to="/" className="uppercase p-2 hover:bg-yellow-200">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="uppercase p-2 hover:bg-yellow-200">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="uppercase p-2 hover:bg-yellow-200">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="uppercase p-2 hover:bg-yellow-200">
+              Solutions
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="uppercase p-2 hover:bg-yellow-200">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,28 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import Container from "../components/ui/Container"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hello people</h1>
-    <p>Welcome to .</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <Container fluid>
+      <h1>Hello people</h1>
+      <p>Welcome to .</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        alt="A Gatsby astronaut"
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+        <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+        <Link to="/using-dsg">Go to "Using DSG"</Link>
+      </p>
+    </Container>
   </Layout>
 )
 
