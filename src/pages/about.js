b0b7ce99c3bb501a4c5edd3df2192b7d8bdf7e79
suman <ghoshs91439@gmail.com>
2022-01-10 12:35:53 +0530

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,20 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the About page</h1>
-    <p>Welcome to About Page</p>
+    <StaticImage className="h-1/5	height: 20%"
+        src="../images/clement-helardot-95YRwf6CNw8-unsplash (2).jpg"
+        alt="A computer image"
+      />
+    
+    <h1>MANAGE YOUR
+BUSINESS
+WITH US</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
